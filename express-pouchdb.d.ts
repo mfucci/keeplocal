1b0fbd84a5c0a1f29667e597b4b55a4ba42d6e92
Marco Fucci di Napoli <mfucci@gmail.com>
2022-04-27 10:07:02 -1000

    Server PouchDb over HTTP

---
+++ express-pouchdb.d.ts
@@ -0,0 +1,30 @@
+/** 
+ * Type definitions for express-pouchdb
+ * 
+ * @license
+ * Copyright 2022 Marco Fucci di Napoli (mfucci@gmail.com)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+declare module "express-pouchdb" {
+    import PouchDB from "pouchdb";
+
+    type AppAddOns = {
+        setPouchDB(pouchdb: any),
+        couchConfig: any,
+        couchLogger: any,
+    }
+
+    type Config = {
+        configPath?: string,
+        logPath?: string,
+        inMemoryConfig?: boolean,
+        mode?: 'fullCouchDB' | 'minimumForPouchDB' | 'custom',
+        overrideMode?: {
+            include?: string[],
+            exclude?: string[],
+        }
+    }
+
+    export default function(pouchdb: any, config?: Config): Express & AppAddOns;
+}
